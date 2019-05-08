c63e14b297e8909bf35ea84d9b6f55a58d8c18a7
iwanttobealight <iwanttobealight@yandex.ru>
2019-05-08 11:55:57 +0300

    feat(query-context): changed react-conext to react-shallow-context

--- lib/QueryContext.ts
+++ lib/QueryContext.ts
@@ -1,11 +1,12 @@
 import * as React from 'react'
+import { createPureContext } from 'react-shallow-context';
 
 interface QueryContextValue {
   query: any
   setQuery: (value: any) => void
 }
 
-export const QueryContext = React.createContext<QueryContextValue>({
+export const QueryContext = createPureContext<QueryContextValue>({
   query: {},
   setQuery: () => null,
 })
