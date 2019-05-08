60c5ce83c2546e93e7fe3a647d5d5304dacd2458
iwanttobealight <iwanttobealight@yandex.ru>
2019-05-08 13:21:57 +0300

    fix(query-context): add important value for context

--- lib/QueryContext.ts
+++ lib/QueryContext.ts
@@ -1,12 +1,12 @@
-import * as React from 'react'
-import { createPureContext } from 'react-shallow-context';
+import * as React from 'react';
+import {updateOnlyFor} from 'react-shallow-context';
 
 interface QueryContextValue {
   query: any
   setQuery: (value: any) => void
 }
 
-export const QueryContext = createPureContext<QueryContextValue>({
+export const QueryContext = React.createContext<QueryContextValue>({
   query: {},
   setQuery: () => null,
-})
+}, updateOnlyFor(['query']))
